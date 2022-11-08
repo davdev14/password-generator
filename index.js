1d88a8776b2db5f4c7075be5099261e93d29a11f
davdev14 <105063415+davdev14@users.noreply.github.com>
2022-11-08 19:26:37 +0100

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passwordEl1 = document.getElementById("password-el1")
-let passwordEl2 = document.getElementById("password-el2")
+let passwordEl1 = document.getElementById("passwordEl1")
+let passwordEl2 = document.getElementById("passwordEl2")
 function getRandomPassword(){
   let pass = ""
   let pass2 = ""
@@ -15,3 +15,22 @@ function getRandomPassword(){
   }
 }
 
+function copyDivToClipboard1() {
+  let range = document.createRange();
+  range.selectNode(document.getElementById("passwordEl1"));
+  window.getSelection().removeAllRanges(); // clear current selection
+  window.getSelection().addRange(range); // to select text
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();// to deselect
+  alert("Copied")
+}
+
+function copyDivToClipboard2() {
+  let range = document.createRange();
+  range.selectNode(document.getElementById("passwordEl2"));
+  window.getSelection().removeAllRanges(); // clear current selection
+  window.getSelection().addRange(range); // to select text
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();// to deselect
+  alert("Copied")
+}
